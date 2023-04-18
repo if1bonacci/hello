438c3cc73f5b5f2c309a18791befd38e83617e01
jerry <kea.riland@gmail.com>
2023-04-18 23:15:41 +0300

    update

--- pkg/hasher/hasher_test.go
+++ pkg/hasher/hasher_test.go
@@ -1,10 +1,19 @@
 package hasher
 
 import (
-	"fmt"
 	"testing"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
-func TestCheckPasswordHash() {
-	fmt.Println(CheckPasswordHash("password", "$2a$10$Kt0YB3SgXJuUpek5anTDguHyKXUEbE4EIyzQXrfzYzsNB9ExZflSe"))
+var password = "password"
+
+func TestCheckWrongPasswordHash(t *testing.T) {
+	hash, _ := HashPassword(password)
+
+	if err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)); err != nil {
+		t.Errorf("Hash Password is wrong")
+	}
+
+	// fmt.Println(CheckPasswordHash("password", "$2a$10$Kt0YB3SgXJuUpek5anTDguHyKXUEbE4EIyzQXrfzYzsNB9ExZflSe"))
 }
