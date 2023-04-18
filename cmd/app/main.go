de028124ae866b900526154817df432de471e6d1
jerry <kea.riland@gmail.com>
2023-04-18 19:58:50 +0300

    update

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 
-	"github.com/if1bonacci/lets-go-chat/pkg/hasher/hasher"
+	"github.com/if1bonacci/lets-go-chat/pkg/hasher"
 )
 
 func main() {
-	hasher, err := hasher.HashPassword()
-	fmt.Println("hello, world.")
+	hasher, err := hasher.HashPassword("password")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(hasher)
 }
