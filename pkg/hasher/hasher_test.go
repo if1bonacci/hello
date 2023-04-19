1100fa540a7e2218772250f95a6ba5b7ca7afd82
jerry <kea.riland@gmail.com>
2023-04-20 01:09:20 +0300

    update tests

--- pkg/hasher/hasher_test.go
+++ pkg/hasher/hasher_test.go
@@ -16,3 +16,21 @@ func TestHashPassword(t *testing.T) {
 		t.Errorf("Hash Password is wrong")
 	}
 }
+
+func TestCheckPasswordHash(t *testing.T) {
+	hash, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	status := CheckPasswordHash(password, string(hash))
+
+	if !status {
+		t.Errorf("Password is wrong")
+	}
+}
+
+func TestFailCheckPasswordHash(t *testing.T) {
+	hash, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	status := CheckPasswordHash("password2", string(hash))
+
+	if status {
+		t.Errorf("Password is wrong")
+	}
+}
