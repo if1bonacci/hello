729b5b896430b49a94b917b9842a81cd41ce2bfe
jerry <kea.riland@gmail.com>
2023-04-27 13:36:10 +0300

    add dotenv

--- cmd/chat/chat.go
+++ cmd/chat/chat.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/if1bonacci/lets-go-chat/internal/routing"
+	"github.com/joho/godotenv"
 
 	"github.com/labstack/echo"
 )
@@ -13,6 +17,10 @@ func main() {
 	// Routes
 	routing.InitAuthRoutes(e)
 
+	godotenv.Load()
+	port := os.Getenv("PORT")
+	address := fmt.Sprintf("%s:%s", "0.0.0.0", port)
+
 	// Start server
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(address))
 }
