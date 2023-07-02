cdde4ddc8a657099268ce33fc4a925ae3f989fb1
jerry <kea.riland@gmail.com>
2023-07-02 22:02:36 +0300

    update

--- pprof.sh
+++ pprof.sh
@@ -1,5 +1,5 @@
 curl localhost:8080/debug/pprof/heap -o mem_out.txt
 curl localhost:8080/debug/pprof/profile?seconds=15 -o cpu_out.txt
 
-go tool pprof -svg -alloc_objects main.exe mem_out.txt > mem_ao.svg
-go tool pprof -svg main.exe cpu_out.txt > cpu.svg
+go tool pprof -svg -alloc_objects main mem_out.txt > mem_ao.svg
+go tool pprof -svg main cpu_out.txt > cpu.svg
